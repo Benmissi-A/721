f21b08f9b60edffeb8393bd28537df05d654cd4f
Benmissi-A <aminebenmissi@hotmail.fr>
2023-04-22 15:36:17 +0200

    ERC721

---
+++ src/Label.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.13;
+
+import {Ownable} from "openzeppelin-contracts/access/Ownable.sol";
+import{ERC721} from "openzeppelin-contracts/token/ERC721/ERC721.sol";
+import{Counters} from "openzeppelin-contracts/utils/Counters.sol";
+contract Label is ERC721, Ownable{
+
+}
