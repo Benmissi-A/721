64945bb68fd815e427d398b090d0109c861e9377
Benmissi-A <aminebenmissi@hotmail.fr>
2023-04-22 16:18:15 +0200

    721

--- src/Label.sol
+++ src/Label.sol
@@ -5,6 +5,15 @@ pragma solidity ^0.8.13;
 import {Ownable} from "openzeppelin-contracts/access/Ownable.sol";
 import{ERC721} from "openzeppelin-contracts/token/ERC721/ERC721.sol";
 import{Counters} from "openzeppelin-contracts/utils/Counters.sol";
-contract Label is ERC721, Ownable{
 
+contract Label is ERC721, Ownable{
+    using Counters for Counters.Counter;
+    Counters.Counter private _IdCounter;
+    constructor() ERC721("label","LBL"){}
+
+    function safeMint(address to, string memory uri) public onlyOwner {
+        uint256 Id = _IdCounter.current();
+        _IdCounter.increment();
+        _safeMint(Owner,Id);
+    }
 }
